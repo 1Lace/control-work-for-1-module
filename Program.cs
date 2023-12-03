3d3f3f070a2a525f25cae7a3ea9c6ac83915bbc0
VIBoss <viboss1337@gmail.com>
2023-12-04 00:37:11 +0300

    added ф-ю ввода кол-во строк

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Len (string message) // ф-я ввода кол-ва строк которые будет вводит П(пользователь)
+{
+    Console.WriteLine(message); // Здесь мы можем гапистаь сообщение перед тем как П сможет что то ввести
+    string value = Console.ReadLine(); // считывает с ввод с консоли
+    int result = Convert.ToInt32(value); // преобразует строку в int
+    return result;
+}
+
+string Str (string message) // ф-я ввода самих строк
+{
+    Console.WriteLine(message);
+    string result = Console.ReadLine(); // считывает с ввод с консоли
+    return result;
+}
