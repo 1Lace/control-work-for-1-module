5b7aae3be68f41199d227b4081096e331ec14a14
VIBoss <viboss1337@gmail.com>
2023-12-04 12:44:21 +0300

    v1.2

--- Program.cs
+++ Program.cs
@@ -3,8 +3,7 @@
 int Len (string message) // ф-я ввода кол-ва строк которые будет вводит П(пользователь)
 {
     Console.WriteLine(message); // Здесь мы можем гапистаь сообщение перед тем как П сможет что то ввести
-    string value = Console.ReadLine(); // считывает с ввод с консоли
-    int result = Convert.ToInt32(value); // преобразует строку в int
+    int result = Convert.ToInt32(Console.ReadLine()); // считывает с ввод с консоли и преобразует строку в int
     return result;
 }
 
